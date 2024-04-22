7a8e776ca537734872dbb72a155b6219145d4ccb
Xanto14 <tomlgthib@gmail.com>
2024-04-21 22:47:23 -0400

    fixed knight

--- Chess_project/Game.h
+++ Chess_project/Game.h
@@ -159,14 +159,14 @@ namespace modele {
 				make_pair(pos.first + 1,pos.second - 2),
 				make_pair(pos.first + 2,pos.second - 1),
 				make_pair(pos.first - 2,pos.second - 1),
-				make_pair(pos.first - 1,pos.second - 2), };
+				make_pair(pos.first - 1,pos.second - 2)};
 			vector<pair<int, int>> moveSetValid;
 			for (const auto& position : moveSet) {
 				if (isPositionInBoard(position))
 					if (board_.getPieceAtPos(position)->getColor() != board_.getPieceAtPos(pos)->getColor())//si on verifie pour une piece mais qu'on est pas sur que la position existe, on va avoir une erreur donc on doit separer
 						moveSetValid.push_back(position);
-				return moveSetValid;
 			}
+			return moveSetValid;
 		}
 
 		vector<pair<int, int>> getMoveValidRook(pair<int, int> pos) {
